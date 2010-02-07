f30f0b80c1733aff7d810ab5b6928f3f6c42f549
RishiD <rishid@gmail.com>
2010-02-06 20:59:41 -0500

    new python install script

---
+++ install.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+ 
+import os
+import shutil
+import optparse
+import sys
+
+def main():
+    def exclude_list(option, opt_str, value, parser):
+        parser.values.exclude = parser.values.exclude + value.split(',')
+        
+    parser = optparse.OptionParser(usage='Usage: %prog [OPTIONS]', description='Install all files and directories as dotfiles.')
+    parser.add_option('-b', '--backup', action='store_true', dest='backup', default=False, help='Backup old files')
+    parser.add_option('-c', '--copy', action='store_true', dest='copy', default=False, help='Copy files instead of hard-linking them')
+    parser.add_option('-e', '--exclude', action='callback', callback=exclude_list, type='string', dest='exclude', default=['README', 'install.py', '.git'], help='Comma seperated list of files to exclude', metavar='FILES')
+    parser.add_option('-f', '--force', action='store_true', dest='force', default=False, help='Overwrite without confirmation')
+    parser.add_option('-p', '--prefix', action='store', dest='prefix', default=os.path.expanduser('~'), help='Install location [default: %default]', metavar='PATH')
+ 
+    (options, args) = parser.parse_args()
+ 
+    def traverse(path):
+        for item in os.listdir(path):
+            source = os.path.join(path, item)
+            
+            #relative = os.path.relpath(source)
+
+            if item.startswith('.'):
+                destination = os.path.join(options.prefix, item)
+            else:
+                destination = os.path.join(options.prefix, '.' + item)
+                    
+            if item in options.exclude:
+                continue
+                    
+            if os.path.isfile(source):
+                print source, "-->", destination
+                install(source, destination)
+                    
+            if os.path.isdir(source):
+                #print source, "is directory"
+                #if not os.path.exists(destination):
+                #    os.mkdir(destination)
+                                
+                #traverse(source)
+                install(source, destination)
+
+    def install(source, destination):
+        if os.path.exists(destination):
+            if options.backup:
+                shutil.move(destination, destination + '.bak')
+            elif options.force:
+                os.remove(destination)
+            else:
+                answer = raw_input(destination + ': File already exists, overwrite? [y/N]')
+
+                if answer.lower() in ('y', 'yes'):
+                    os.remove(destination)
+                else:
+                    return
+                
+        if options.copy:
+            shutil.copy(source, destination)
+        else:
+            os.symlink(source, destination)
+                                                
+    traverse(os.path.dirname( os.path.realpath( __file__ ) ) )
+    #traverse(os.getcwd())
+                                                
+if __name__ == '__main__':
+    try:
+        main()
+        print os.getcwd()
+        print os.path.dirname( os.path.realpath( __file__ ) )
+    except (KeyboardInterrupt, SystemExit):
+        sys.exit(1)
+                                                        
