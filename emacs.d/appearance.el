56502a66e79de61bfa3444a0d4e33a0aacb5fd14
RishiD <rishid@gmail.com>
2012-01-31 19:41:17 -0700

    more refactoring

---
+++ emacs.d/appearance.el
@@ -0,0 +1,55 @@
+(setq visible-bell t
+      font-lock-maximum-decoration t
+      color-theme-is-global t
+      truncate-partial-width-windows nil)
+
+;(set-face-background 'region "#464740")
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Customize background color of lighlighted line
+;(set-face-background 'hl-line "#222222")
+
+;; Subtler highlight in magit
+;(set-face-background 'magit-item-highlight "#121212")
+;(set-face-foreground 'magit-diff-none "#666666")
+;(set-face-foreground 'magit-diff-add "#00cc33")
+
+;; Highlight in yasnippet
+(set-face-background 'yas/field-highlight-face "#333399")
+
+;; org-mode colors
+(setq org-todo-keyword-faces
+      '(
+        ("INPR" . (:foreground "yellow" :weight bold))
+        ("DONE" . (:foreground "green" :weight bold))
+        ("IMPEDED" . (:foreground "red" :weight bold))
+        ))
+
+;; Highlight matching parentheses when the point is on them.
+;; show-paren-mode: subtle blinking of matching paren (defaults are ugly)
+;; http://www.emacswiki.org/cgi-bin/wiki/ShowParenMode
+(show-paren-mode t)
+(setq show-paren-delay 0.0)
+(setq show-paren-style 'mixed)
+
+;; No menu bars
+(menu-bar-mode -1)
+
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  (turn-off-tool-bar)
+  (tooltip-mode -1)
+  (turn-off-tool-bar)
+  (blink-cursor-mode -1))
+
+(add-hook 'before-make-frame-hook 'turn-off-tool-bar)
+
+;; Ditch them scrollbars
+(scroll-bar-mode -1)
+
+;; Make zooming affect frame instead of buffers
+(require 'zoom-frm)
+
+(provide 'appearance)
---
+++ emacs.d/sane-defaults.el
@@ -0,0 +1,84 @@
+;; Allow pasting selection outside of Emacs
+(setq x-select-enable-clipboard t)
+
+;; Auto refresh buffers
+;(global-auto-revert-mode 1)
+
+;; Also auto refresh dired
+(setq global-auto-revert-non-file-buffers t)
+
+;; Show keystrokes in progress
+(setq echo-keystrokes 0.1)
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+;; Move files to trash when deleting
+(setq delete-by-moving-to-trash t)
+
+;; Real emacs knights don't use shift to mark things
+(setq shift-select-mode nil)
+
+;; Transparently open compressed files
+(auto-compression-mode t)
+
+;; Enable syntax highlighting for older Emacsen that have it off
+(global-font-lock-mode t)
+
+;; Answering just 'y' or 'n' will do
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; UTF-8 please
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; Show active region
+(transient-mark-mode 1)
+(make-variable-buffer-local 'transient-mark-mode)
+(put 'transient-mark-mode 'permanent-local t)
+(setq-default transient-mark-mode t)
+
+;; Remove text in active region if inserting text
+(delete-selection-mode 1)
+
+;; Always display line and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+;; Lines should be 80 characters wide, not 72
+(setq fill-column 80)
+
+;; Save a list of recent files visited.
+(require 'recentf)
+(setq recentf-save-file (concat emacs-tmp-dir "/recentf")  ;; keep ~/ clean
+	  recentf-max-saved-items 100                          ;; max save 100
+	  recentf-max-menu-items 15)                           ;; max 15 in menu
+(recentf-mode t)                                           ;; turn it on
+
+;; Never insert tabs
+(setq-default 'indent-tabs-mode nil)
+
+;; Show me empty lines after buffer end
+(setq-default 'indicate-empty-lines t)
+
+;; Don't break lines for me, please
+;(setq-default truncate-lines t)
+
+;; Keep cursor away from edges when scrolling up/down
+(require 'smooth-scrolling)
+
+;; Represent undo-history as an actual tree (visualize with C-x u)
+(setq undo-tree-mode-lighter "")
+(require 'undo-tree)
+(global-undo-tree-mode)
+
+;; Add parts of each file's directory to the buffer name if not unique
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward
+      uniquify-after-kill-buffer-p t
+      uniquify-ignore-buffers-re "^\\*")
+
+(provide 'sane-defaults)
