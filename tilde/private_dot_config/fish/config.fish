ab9c793a036caa2e392bfabe8f6ff21ef3c90249
Rishi Dhupar <rishid@gmail.com>
2023-09-16 08:38:37 -0400

    Add .chezmoi.yaml Update ansible/roles/dotfiles/tasks/main.yml Add tilde/.chezmoi.yaml Remove tilde/fish/.config/fish/config.fish Add tilde/.config/.gitignore Add tilde/.config/fish/config.fish Change attributes of tilde/.bash_profile Change attributes of tilde/.bashrc Change attributes of tilde/.config/curlrc Change attributes of tilde/.config/fish/fish_plugins

---
+++ tilde/private_dot_config/fish/config.fish
@@ -0,0 +1,16 @@
+source "$__fish_config_dir/variables.fish"
+
+# automatically loads plugins under "$fisher_path"
+source "$__fish_config_dir/functions/fisher_path.fish"
+
+# Install fisher, if not exist
+if status is-interactive && ! functions --query fisher
+  curl -sL https://git.io/fisher | source && fisher update
+end
+
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  function fish_greeting
+    echo ~~~  Hello (date +%A)!  ~~~
+  end
+end
