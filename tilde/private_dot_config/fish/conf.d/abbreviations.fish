718beb7bd3affb2be0c1271fb835aa2397754d72
Rishi Dhupar <rishid@gmail.com>
2023-09-20 17:23:25 -0400

    Update tilde/.chezmoiscripts/install-docker.sh Add tilde/.chezmoitemplates/docker_daemon Update tilde/.config/fish/conf.d/abbreviations.fish

--- tilde/private_dot_config/fish/conf.d/abbreviations.fish
+++ tilde/private_dot_config/fish/conf.d/abbreviations.fish
@@ -5,8 +5,8 @@ abbr da "direnv allow"
 abbr dc "docker-compose"
 
 alias ls="eza --icons"
-abbr ll="eza -l -g --icons --git"
-abbr lla="eza -la -g --icons --git"
-abbr llt="eza -1 --icons --tree --git-ignore"
+abbr ll "eza -l -g --icons --git"
+abbr lla "eza -la -g --icons --git"
+abbr llt "eza -1 --icons --tree --git-ignore"
 
 abbr tf "terraform"
