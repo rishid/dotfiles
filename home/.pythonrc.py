fdfdf6486e2df451ad1986c3aeb4706317129521
Rishi Dhupar <rishid@gmail.com>
2023-09-20 10:51:37 -0400

    python startup file

---
+++ home/.pythonrc.py
@@ -0,0 +1,6 @@
+# dir() alternative http://inky.github.com/see
+try:
+    from see import see
+except ImportError:
+    print('\n`see()` not found, get it at http://inky.github.com/see or easy_install -U see\n')
+
