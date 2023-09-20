9fb7dc541ff822b6fb6267995020ba31fa0402b4
Rishi Dhupar <rishid@gmail.com>
2023-09-20 10:51:36 -0400

    seperating key bindings to a new file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,6 @@
 ;; init.el file
 ;; Rishi Dhupar
-;; Time-stamp: <05-25-2011 09:54:51 (rkd4127)>
+;; Time-stamp: <09-01-2011 16:54:53 (rkd4127)>
 
 ;; Check for Linux and start the server
 ;(if (string-equal system-type "gnu/linux")
@@ -276,65 +276,7 @@
      (color-theme-almost-monokai)))) 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; global keybindings
-(defmacro djcb-program-shortcut (name key &optional use-existing)
-  "* macro to create a key binding KEY to start some terminal program PRG; 
-    if USE-EXISTING is true, try to switch to an existing buffer"
-  `(global-set-key ,key 
-     '(lambda() (interactive) 
-        (djcb-term-start-or-switch ,name ,use-existing))))
-
-(global-set-key (kbd "<delete>")    'delete-char)  ; delete == delete    
-(global-set-key (kbd "M-g")         'goto-line)    ; M-g  'goto-line
-;; kill whole line with C-; (because ; is close to k)
-(global-set-key (kbd "C-;") 'kill-whole-line)
-
-;; C-pgup goes to the start, C-pgdw goes to the end
-(global-set-key(kbd "<C-prior>")(lambda()(interactive)(goto-char(point-min))))
-(global-set-key(kbd "<C-next>") (lambda()(interactive)(goto-char(point-max))))
-
-(global-set-key (kbd "C-z") 'undo)   ;; use it like CUA, not like 'suspend'
-(global-set-key (kbd "s-b") 'pop-global-mark) ; jump *back* to prev location
-
-; replace-string and replace-regexp need a key binding
-(global-set-key (kbd "C-c s") 'replace-string)
-(global-set-key (kbd "C-c r") 'replace-regexp)
-
-;; programming/writing stuff; f5-f8 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-set-key (kbd "<f7>") 'compile)                     ;; compile
-(global-set-key (kbd "S-<f7>") 'delete-other-windows)      ;; close ...
-
-
-(global-set-key (kbd "<f8>") 'comment-or-uncomment-region) ;; (un)comment
-;; for writing
-(global-set-key (kbd "C-<f5>") 'djcb-count-words)          ;; count words
-(when (djcb-require-maybe 'magit)                          ;; marius...
-  (global-set-key (kbd "C-<f6>") 'magit-status))           ;; ...git mode
-;; some toggles; Shift + function key
-(global-set-key (kbd "<S-f6>") 'highlight-changes-visible-mode) ;; changes
-(global-set-key (kbd "<S-f7>") 'whitepace-mode)            ;; show blanks
-(autoload 'linum "linum" "mode for line numbers" t) 
-(global-set-key (kbd "<S-f9>")   'djcb-fullscreen-toggle)  ;; fullscreen
-(global-set-key (kbd "<S-<f10>")  'package-list-packages)  ;; elpa
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;(global-set-key (kbd "M-X") 'smex) ;; use smex
-;; this depends on smex availability                                              
-;(setq smex-save-file (concat djcb-tmp-dir "/smex.save"))              
-;(when (djcb-require-maybe 'smex)                                                  
-;  (smex-initialize))                                                           
-
-;; hippy expands starts with try-yasnippet-expand
-(global-set-key [(control tab)] 'hippie-expand) ; Ctrl-Tab for expand
-
-;; tab is for indentation, not completion
-(global-set-key (kbd "TAB") 'indent-for-tab-command)
-(setq-default indent-tabs-mode nil)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'bindings)
 
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; yasnippet 
