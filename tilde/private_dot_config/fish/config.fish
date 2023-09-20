d9826cbaa1fb9ef01239e74a466fe5891f7ce374
Rishi Dhupar <rishid@gmail.com>
2023-09-20 11:35:22 -0400

    Update tilde/.config/fish/config.fish Add tilde/.config/fish/functions/fish_greeting.fish

--- tilde/private_dot_config/fish/config.fish
+++ tilde/private_dot_config/fish/config.fish
@@ -19,13 +19,6 @@ if status is-interactive && ! functions --query fisher
   curl -sL https://git.io/fisher | source && fisher update
 end
 
-if status is-interactive
-  # Commands to run in interactive sessions can go here
-  function fish_greeting
-    echo ~~~  Hello (date +%A)!  ~~~
-  end
-end
-
 fish_add_path --global "$HOME/.local/bin/"
 
 set -Ux EDITOR emacs
