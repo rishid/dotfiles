6acabc58eb94270f0a55c8098ea80966a71315ad
Rishi Dhupar <rishid@gmail.com>
2023-09-20 14:54:33 -0400

    add keychain support

--- tilde/private_dot_config/fish/conf.d/abbreviations.fish
+++ tilde/private_dot_config/fish/conf.d/abbreviations.fish
@@ -4,7 +4,9 @@ abbr d "docker"
 abbr da "direnv allow"
 abbr dc "docker-compose"
 
-abbr l "ls -la"
-abbr ls "eza --icons"
+alias ls="eza --icons"
+abbr ll="eza -l -g --icons --git"
+abbr lla="eza -la -g --icons --git"
+abbr llt="eza -1 --icons --tree --git-ignore"
 
 abbr tf "terraform"
