921dff4660ae69664c0db94a550702c5cb3aa1ed
Rishi Dhupar <rishid@gmail.com>
2023-09-20 10:51:38 -0400

    Small tweaks to init script

--- bin/init.sh
+++ bin/init.sh
@@ -3,6 +3,9 @@
 # echo every command and exit if any command fails
 set -ex
 
+echo "Ask for sudo password"
+sudo -v
+
 # update in case this is done on a brand new system that has not updated
 sudo apt-get update
 
@@ -20,3 +23,18 @@ else
   git clone https://github.com/rishid/dotfiles.git ~/.dotfiles
 fi
 
+# backup bashrc and bash_profile
+if [ -f "$HOME/.bashrc" ] && [ ! -h "$HOME/.bashrc" ]
+then
+    echo "[i] Move current ~/.bashrc to ~/bashrc_backup"
+    mv "$HOME/.bashrc" "$HOME/bashrc_backup"
+fi
+
+if [ -f "$HOME/.bash_profile" ] && [ ! -h "$HOME/.bash_profile" ]
+then
+    echo "[i] Move current ~/.bash_profile to ~/bash_profile_backup"
+    mv "$HOME/.bash_profile" "$HOME/bash_profile_backup"
+fi
+
+echo "From now on you can use $ dotfiles to manage your dotfile"
+echo "Done"
