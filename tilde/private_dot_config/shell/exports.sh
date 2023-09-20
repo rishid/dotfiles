826766e6b2309172951cad4c3eb2833ad08de9b2
Rishi Dhupar <rishid@gmail.com>
2023-09-20 10:53:34 -0400

    Update tilde/.config/shell/exports.sh

--- tilde/private_dot_config/shell/exports.sh
+++ tilde/private_dot_config/shell/exports.sh
@@ -18,8 +18,7 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 
-
-### Exports
+### Paths
 export PATH="/opt/local/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
