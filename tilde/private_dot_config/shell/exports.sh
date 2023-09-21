b873ed09b40a0560cb328c9cdc488dde8c14716b
Rishi Dhupar <rishid@gmail.com>
2023-09-21 11:31:17 -0400

    Update tilde/.config/shell/exports.sh

--- tilde/private_dot_config/shell/exports.sh
+++ tilde/private_dot_config/shell/exports.sh
@@ -67,6 +67,7 @@ export SCREENRC="$XDG_CONFIG_HOME"/screen/screenrc
 export WGETRC="$XDG_CONFIG_HOME/wget/wgetrc"
 
 ### History
+# TODO: move to bash specific file
 export HISTCONTROL=ignoreboth
 export HISTSIZE=1000000000
 export HISTFILESIZE=$HISTSIZE
@@ -77,12 +78,13 @@ export HISTIGNORE="&:[bf]g:c:clear:history:exit:q:afk:pwd:* --help"
 export SAVEHIST=50000
 
 ### Man pages
-export LESS_TERMCAP_mb=$'\e[1;32m'
-export LESS_TERMCAP_md=$'\e[1;32m'
-export LESS_TERMCAP_me=$'\e[0m'
-export LESS_TERMCAP_se=$'\e[0m'
-export LESS_TERMCAP_so=$'\e[01;33m'
-export LESS_TERMCAP_ue=$'\e[0m'
-export LESS_TERMCAP_us=$'\e[1;4;31m'
-export LESSHISTFILE=-
-export MANPAGER="less -X"
+# TODO: move to bash specific file
+# export LESS_TERMCAP_mb=$'\e[1;32m'
+# export LESS_TERMCAP_md=$'\e[1;32m'
+# export LESS_TERMCAP_me=$'\e[0m'
+# export LESS_TERMCAP_se=$'\e[0m'
+# export LESS_TERMCAP_so=$'\e[01;33m'
+# export LESS_TERMCAP_ue=$'\e[0m'
+# export LESS_TERMCAP_us=$'\e[1;4;31m'
+# export LESSHISTFILE=-
+# export MANPAGER="less -X"
