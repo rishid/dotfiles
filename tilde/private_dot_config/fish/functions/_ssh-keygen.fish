bbcae68bca16e60d2d931dfe3223f0b5bd30f522
Rishi Dhupar <rishid@gmail.com>
2024-01-05 14:20:38 -0500

    Update _ssh-keygen.fish

--- tilde/private_dot_config/fish/functions/_ssh-keygen.fish
+++ tilde/private_dot_config/fish/functions/_ssh-keygen.fish
@@ -1,6 +1,6 @@
 function _ssh-keygen
   if not test -f ~/.ssh/id_ed25519
-    ssh-keygen -t ed25519 -a 100 -f ~/.ssh/id_ed25519 -C "$USER@$HOST $(date -u '+%FT%TZ')"
+    ssh-keygen -t ed25519 -a 100 -f ~/.ssh/id_ed25519 -C "$(whoami)@$(uname -n) $(date -u '+%FT%TZ')"
   else
     echo "Key file ~/.ssh/id_ed25519 already exists."
   end
