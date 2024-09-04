9d11899e88f9a23c7f5ac7cffdc0ab09875070d2
Rishi Dhupar <rishid@gmail.com>
2024-09-04 11:26:19 -0400

    Updates

---
+++ tilde/private_dot_config/fish/functions/shellcheck.fish
@@ -0,0 +1,17 @@
+function shellcheck -d "Runs shellcheck through the system or docker depending on how it is available"
+    if command -q shellcheck
+        command shellcheck $argv
+    # else if test -x $BIN_FOLDER/shellcheck
+    #    $BIN_FOLDER/shellcheck $argv
+    else if command -q docker
+        docker run \
+            --rm \
+            --tty \
+            --volume="$PWD:/mnt" \
+            docker.io/koalaman/shellcheck:latest \
+            $argv
+    else
+        print_error "shellcheck could not be found."
+        return 1
+    end
+end
