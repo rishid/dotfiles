73787ba40749c6a5a029fe284b36cbb1d4e12c9c
Rishi Dhupar <rishid@gmail.com>
2025-04-04 18:33:42 -0400

    need to set XDG dirs before sourcing exports.sh

--- tilde/private_dot_config/fish/conf.d/000-init.fish
+++ tilde/private_dot_config/fish/conf.d/000-init.fish
@@ -1,2 +1,9 @@
 # By dictionary order, this file is sourced first in fish shell
 # Define user-specific environment variables for fish
+
+# Set XDG basedirs.
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+set -q XDG_CONFIG_HOME; or set -Ux XDG_CONFIG_HOME $HOME/.config
+set -q XDG_DATA_HOME; or set -Ux XDG_DATA_HOME $HOME/.local/share
+set -q XDG_STATE_HOME; or set -Ux XDG_STATE_HOME $HOME/.local/state
+set -q XDG_CACHE_HOME; or set -Ux XDG_CACHE_HOME $HOME/.cache
