e308ac0f5058ac5abe938ec07cb1f5a23e70a1ce
Rishi Dhupar <rishid@gmail.com>
2026-03-14 07:17:08 -0400

    Update .chezmoi.yaml Update .config/fish/functions/ce.fish

--- tilde/private_dot_config/fish/functions/ce.fish
+++ tilde/private_dot_config/fish/functions/ce.fish
@@ -1,6 +1,7 @@
 function ce -d "Fuzzy-find and edit a chezmoi-managed file"
 	set SEARCH_RESULT (chezmoi managed --exclude=externals --include=files | fzf)
 	if test $status -eq 0
+        echo "foobar"
 		chezmoi edit --watch --apply $HOME/$SEARCH_RESULT
 	end
 end
