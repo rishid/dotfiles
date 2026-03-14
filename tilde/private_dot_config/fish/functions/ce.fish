93b9f41e65f8ea5550b38fe5096504f63c3ebe98
Rishi Dhupar <rishid@gmail.com>
2026-03-14 07:44:37 -0400

    fix(ce): add query support for fuzzy-find in chezmoi-managed files

--- tilde/private_dot_config/fish/functions/ce.fish
+++ tilde/private_dot_config/fish/functions/ce.fish
@@ -1,5 +1,5 @@
 function ce -d "Fuzzy-find and edit a chezmoi-managed file"
-    set SEARCH_RESULT (chezmoi managed --exclude=externals --include=files | fzf --prompt="chezmoi> ")
+    set SEARCH_RESULT (chezmoi managed --exclude=externals --include=files | fzf --prompt="chezmoi> " --query=(string join " " $argv))
     test $status -eq 0 || return 0
 
     set target "$HOME/$SEARCH_RESULT"
